94d07f75b27fbe0dd6bde26cd1dc9d6d7fa9e46d
Oscar Arzola <oscararzola@gmail.com>
2014-11-29 00:00:19 -0600

    T.2

---
+++ semana4/generators.php
@@ -0,0 +1,37 @@
+<?php
+
+$start_time = microtime(true);
+$array = array();
+$result = '';
+for ($count = 1000000; $count--;) {
+    $array[] = $count / 2;
+}
+foreach ($array as $val) {
+    $val += 145.56;
+    $result .= $val;
+}
+$end_time = microtime(true);
+
+echo "time: ", bcsub($end_time, $start_time, 4), "\n";
+echo "memory (byte): ", memory_get_peak_usage(true), "\n";
+?>
+
+<?php
+
+$start_time = microtime(true);
+$result = '';
+
+function it() {
+    for ($count = 1000000; $count--;) {
+        yield $count / 2;
+    }
+}
+
+foreach (it() as $val) {
+    $val += 145.56;
+    $result .= $val;
+}
+$end_time = microtime(true);
+
+echo "time: ", bcsub($end_time, $start_time, 4), "\n";
+echo "memory (byte): ", memory_get_peak_usage(true), "\n";
